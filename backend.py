605c071788fdaca0c38692b8b0559b252a58ac17
kaif-00 <mdkaifkhan6904@gmail.com>
2026-08-01 17:16:42 +0530

    twelth commit

--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect,url_for,flash
 from dotenv import load_dotenv
 from openai import OpenAI
 from ollama import chat
+from pyngrok import ngrok,conf
 import json
 import os
 import re
@@ -309,7 +310,7 @@ def show_task():
                 continue
 
         else:
-            files = ['complete.txt','skip.txt','question.json','question.txt','level.json','xp.txt']
+            files = ['complete.txt','skip.txt','question.json','question.txt','level.json']
             remove_file(files)
             return render_template('final.html')
 
@@ -524,5 +525,20 @@ def pending_task():
     return render_template('pending_task.html',
                            tasks = tasks, content = content)
 
+
+conf.get_default().auth_token='3Ga0kzoko9TRMvUZOzCWgV9iiNz_51DugyUD4sDdCzxrRJFcd'
+
+tunnels = ngrok.get_tunnels()
+print(f'Active tunnels: {tunnels}')
+for tunnel in tunnels:
+    print(f'Closing: {tunnel.public_url}')
+    ngrok.disconnect(tunnel.public_url)
+
+ngrok.kill()
+print('All tunnels closed')
 if __name__ == '__main__':
-    backend.run(debug=False, port=5000)
+    if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+        public_url = ngrok.connect(5000)
+        print(f'Open this on your phone: {public_url}')
+    
+    backend.run(debug=True, port=5000)
